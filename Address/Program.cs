b38115137f341a48c7ff547aa82c074699f97a99
Nikita <nikitasaran155@gmail.com>
2022-10-29 00:37:35 +0300

    address project

---
+++ Address/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address
+            {
+                Index = "02224",
+                Country = "Ukraine",
+                City = "Kiyv",
+                Street = "Balsaka st.",
+                House = "8A",
+                Appartment = 41
+            };
+
+            Console.WriteLine(address.ToString());
+        }
+    }
+}
