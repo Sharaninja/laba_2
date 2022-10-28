1d4948729365b7e98f9badb7fdfe41c56425068b
Nikita <nikitasaran155@gmail.com>
2022-10-29 00:52:05 +0300

    Employee project

---
+++ Employee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("John", "Weak", 3.5, Position.Middle);
+            Console.WriteLine($"Salary: {employee.Salary,0:0.00}");
+            Console.WriteLine($"Tax: {employee.Tax}");
+        }
+    }
+}
