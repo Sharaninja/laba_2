cc2d1a0b7554f31c10dee1c0766da4a8edd97a67
Nikita <nikitasaran155@gmail.com>
2022-10-29 00:56:39 +0300

    User project

---
+++ User/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Albert", "Einstein", 38, "Apple123");
+            Console.WriteLine(user.ToString());
+        }
+    }
+}
